b87dd1202d19fd442a5f2fc3f58dfddf7542d852
s0Xu-git <romanobuhov49@gmail.com>
2025-10-21 19:39:58 +0300

    Create hendlers.py

---
+++ hendlers.py
@@ -0,0 +1,15 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+
+token_ai = "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" # Не трогать
+token_bot = "8244062672:AAEF1BxLpx6HHfekvwJJ1V4lrjENx2r_Q9c"
+
+async def main():
+    bot = Bot(token=token_bot)
+    dp = Dispatcher()
+
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
