50a847d86713f093ba77e90a105a8b1da1390dd0
s0Xu-git <romanobuhov49@gmail.com>
2025-10-23 00:19:26 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher
-from hendlers import router
+
+from hendlers import *
 
 token_ai = "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" # Не трогать
 token_bot = "8244062672:AAEF1BxLpx6HHfekvwJJ1V4lrjENx2r_Q9c" # получи токен в @BotFather
