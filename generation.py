78f9995d8ac4d9e38df10c2a630bc15a7bb3cdad
s0Xu-git <romanobuhov49@gmail.com>
2025-10-23 00:04:46 +0300

    Update generation.py

--- generation.py
+++ generation.py
@@ -1,18 +1,20 @@
-from openai import OpenAI
-from hendlers import token_ai
+from openai import AsyncOpenAI
+from run import token_ai
 
-client = OpenAI(
+client = AsyncOpenAI(
   base_url="https://openrouter.ai/api/v1",
   api_key=token_ai,
 )
 
-completion = client.chat.completions.create(
-  model="deepseek/deepseek-chat-v3.1",
-  messages=[
-    {
-      "role": "user",
-      "content": "What is the meaning of life?"
-    }
-  ]
-)
-print(completion.choices[0].message.content)
+async def generate(text: str):
+  completion = await client.chat.completions.create(
+    model="deepseek/deepseek-chat-v3.1",
+    messages=[
+      {
+        "role": "user",
+        "content": text
+      }
+    ]
+  )
+  print(completion)
+  return completion.choices[0].message.content
