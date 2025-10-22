63807486aa697fd019bed3ef04ef82e7ae09c7ae
s0Xu-git <romanobuhov49@gmail.com>
2025-10-23 00:04:53 +0300

    Update hendlers.py

--- hendlers.py
+++ hendlers.py
@@ -1,15 +1,30 @@
-import asyncio
-from aiogram import Bot, Dispatcher
+from aiogram import Router
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
 
-token_ai = "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" # Не трогать
-token_bot = "8244062672:AAEF1BxLpx6HHfekvwJJ1V4lrjENx2r_Q9c"
+from generation import generate
 
-async def main():
-    bot = Bot(token=token_bot)
-    dp = Dispatcher()
+router = Router()
 
-    await dp.start_polling(bot)
+class Gen(StatesGroup):
+    wait = State()
 
-if __name__ == '__main__':
-    asyncio.run(main())
+
+@router.message(CommandStart())
+async def main(message: Message):
+    await message.answer("Привет! Я твой персональный помощник в учебном процессе. Ты можешь задать вопрос по интересующей теме!")
+
+@router.message(Gen.wait)
+async def anti_flood(message: Message):
+    await message.answer("Дождитесь окончание генерации прошлого процесса!")
+
+@router.message()
+async def request_processing(message: Message, state: FSMContext):
+    await state.set_state(Gen.wait)
+
+    responce = await generate(message.text)
+    await message.reply(responce)
+    await state.clear()
 
